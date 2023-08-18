ad1a6eeac35d46abefbc7da5fb1ebfdf3196d2d4
Arjun Savel <35353555+arjunsavel@users.noreply.github.com>
2023-08-17 21:40:52 -0400

    Update __init__.py

---
+++ src/opac_compress/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__uri__ = "https://github.com/arjunsavel/cortecs"
+__author__ = "Arjun B. Savel, Megan Bedell, and Eliza M.-R. Kempton"
+__email__ = "asavel@gmail.com"
+__license__ = "MIT"
+__version__ = "0.0.1-beta"
+__release__ = "0.0.1-beta"
+__description__ = "Compressed opacity for radiative transfer of exoplanet cross-correlation spectroscopy"
