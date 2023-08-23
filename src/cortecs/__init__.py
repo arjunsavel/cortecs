4ac32f1a962026927006edbea96b1db011085ec1
arjunsavel <asavel@gmail.com>
2023-08-23 14:27:31 -0400

    oops! move init to correct directory.

---
+++ src/cortecs/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__uri__ = "https://github.com/arjunsavel/cortecs"
+__author__ = "Arjun B. Savel, Megan Bedell, Eliza M.-R. Kempton"
+__email__ = "asavel@gmail.com"
+__license__ = "MIT"
+__version__ = "0.0.1-beta"
+__release__ = "0.0.1-beta"
+__description__ = "Compress opacity for radiative transfer in exoplanet cross-correlation studies"
