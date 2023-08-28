5f4f75ccaa7b92811888b1e3452adf8cc6c5265e
arjunsavel <asavel@gmail.com>
2023-08-27 21:36:03 -0400

    test method funcs and names are assigned correctly

--- src/cortecs/tests/test_fit.py
+++ src/cortecs/tests/test_fit.py
@@ -3,6 +3,7 @@ tests all of the fitting functions.
 """
 import unittest
 from cortecs.fit.fit import *
+from cortecs.fit.fit_pca import *
 
 
 class TestFitter(unittest.TestCase):
@@ -10,7 +11,7 @@ class TestFitter(unittest.TestCase):
     Test the fitter object itself
     """
 
-    def test_available_methods(self):
+    def test_unavailable_methods(self):
         """
         If I pass a method that isn't sypported, an error should be raised.
         :return:
@@ -18,3 +19,23 @@ class TestFitter(unittest.TestCase):
         opac = ""  # not a real Opac object, just a test here
 
         self.assertRaises(ValueError, Fitter, opac, method="wrongmethod")
+
+    def test_available_methods_assigned_name(self):
+        """
+        If I pass a method that isn't sypported, an error should be raised.
+        :return:
+        """
+        opac = ""  # not a real Opac object, just a test here
+
+        fitter = Fitter(opac, method="pca")
+        self.assertEqual(fitter.method, "pca")
+
+    def test_available_methods_assigned_func(self):
+        """
+        If I pass a method that isn't sypported, an error should be raised.
+        :return:
+        """
+        opac = ""  # not a real Opac object, just a test here
+
+        fitter = Fitter(opac, method="pca")
+        self.assertEqual(fitter.fit_func, fit_pca)
