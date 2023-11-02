8f581cacd15db01f63ba0f57fc19b640e6ac10df
arjunsavel <asavel@gmail.com>
2023-11-02 12:17:46 -0400

    add nn integration test

--- src/cortecs/tests/test_integration.py
+++ src/cortecs/tests/test_integration.py
@@ -8,12 +8,19 @@ from cortecs.fit.fit_pca import *
 import os
 import sys
 import numpy as np
+import tensorflow as tf
 
 import cortecs
 from cortecs.opac.opac import *
 from cortecs.fit.fit import *
 from cortecs.fit.fit_pca import *
 from cortecs.eval.eval import *
+import pickle
+
+seed = 42
+
+np.random.seed(seed)
+tf.random.set_seed(seed)
 
 sys.path.insert(0, os.path.abspath("."))
 
@@ -61,3 +68,60 @@ class TestIntegration(unittest.TestCase):
         )
         percent_error = 100 * (eval_res - array_res) / array_res
         self.assertTrue(percent_error < 10)
+
+    def test_nn(self):
+        """
+        basically just the neural net notebook. check that error < 10%.
+        """
+        T_filename = "temperatures.npy"
+        P_filename = "pressures.npy"
+        wl_filename = "wavelengths.npy"
+
+        cross_sec_filename = "absorb_coeffs_C2H4.npy"
+
+        load_kwargs = {
+            "T_filename": T_filename,
+            "P_filename": P_filename,
+            "wl_filename": wl_filename,
+        }
+        opac_obj = Opac(cross_sec_filename, loader="platon", load_kwargs=load_kwargs)
+        fitter = Fitter(opac_obj, method="neural_net")
+        res = cortecs.fit.fit_neural_net.fit_neural_net(
+            fitter.opac.cross_section[:, :, -2],
+            fitter.opac.T,
+            fitter.opac.P,
+            None,
+            all_wl=False,
+            n_layers=3,
+            n_neurons=8,
+            activation="sigmoid",
+            learn_rate=0.04,
+            loss="mean_squared_error",
+            epochs=4000,
+            verbose=0,
+            sequential_model=None,
+            plot=False,
+        )
+        history, neural_network = res
+        P_unraveled = unravel_data(fitter.opac.P, fitter.opac.T, None, tileboth=True)
+        T_unraveled = unravel_data(fitter.opac.T, fitter.opac.P, None, tileboth=False)
+        input_array = np.column_stack([T_unraveled, P_unraveled])
+
+        npres = len(fitter.opac.P)
+        ntemp = len(fitter.opac.T)
+
+        predictions = neural_network.predict(input_array)
+        percent_errors = (
+            100
+            * (predictions.reshape(ntemp, npres) - fitter.opac.cross_section[:, :, -1])
+            / predictions.reshape(ntemp, npres)
+        )
+        median_err = np.median(np.abs(percent_errors))
+        save_neural_network(neural_network, "test_nn.pkl")
+        with open("test_nn.pkl", "rb") as f:
+            all_weights, all_biases = pickle.load(f)
+
+        res1 = n_layers = len(all_weights)
+        eval_neural_net(100, 1e-4, n_layers, all_weights, all_biases)
+        res2 = predictions[0]
+        self.assertTrue(np.isclose(res1, res2) and median_err < 10)
