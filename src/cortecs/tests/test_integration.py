061478e53d4abcacb54882b6da22c78fa0c4f235
arjunsavel <asavel@gmail.com>
2023-11-03 16:51:33 -0400

    try and make the optimizer more deterministic

--- src/cortecs/tests/test_integration.py
+++ src/cortecs/tests/test_integration.py
@@ -8,6 +8,7 @@ from cortecs.fit.fit_pca import *
 import os
 import sys
 import numpy as np
+import random
 import tensorflow as tf
 
 import cortecs
@@ -158,6 +159,8 @@ class TestIntegration(unittest.TestCase):
         # reset random seed
         np.random.seed(seed)
         tf.random.set_seed(seed)
+        os.environ["PYTHONHASHSEED"] = str(seed)
+        random.seed(seed)
         load_kwargs = {
             "T_filename": self.T_filename,
             "P_filename": self.P_filename,
