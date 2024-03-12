122e45b83d14e5086840819e4d06b777d012dcab
arjunsavel <asavel@gmail.com>
2024-03-11 20:55:37 -0400

    remove redundant init info

--- src/cortecs/__init__.py
+++ src/cortecs/__init__.py
@@ -1,9 +1,1 @@
 # -*- coding: utf-8 -*-
-
-__uri__ = "https://github.com/arjunsavel/cortecs"
-__author__ = "Arjun B. Savel, Megan Bedell, Eliza M.-R. Kempton"
-__email__ = "asavel@gmail.com"
-__license__ = "MIT"
-__version__ = "0.3.1"
-__release__ = "0.3.1"
-__description__ = "Compress opacity for radiative transfer"
